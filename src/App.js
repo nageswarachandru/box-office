b0786b984e5ee430afa7d316deb67ca5277389fa
nageswarachandru <cybernag66@gmail.com>
2021-01-09 17:24:18 +0530

    created react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div >
-      Hi
-    </div>
+    <Switch>
+      <Route exact path = "/"> This is avengers house</Route>
+      <Route exact path = "/starred"> I am steve rogus</Route>
+      <Route>Oops,this is 404 error</Route>
+    </Switch>
+    
   );
 }
 
