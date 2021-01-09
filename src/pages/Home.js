64fcb45d4707fe5d24d8fed02a7531652197bcfb
nageswarachandru <cybernag66@gmail.com>
2021-01-09 17:58:18 +0530

    Added navbar and created the page components

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      This is AVENGERS house
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/Starred.js
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react'
+
+const Starred = () => {
+  return (
+    <div>
+      I am steve rogus(CAPTAIN AMERICA)
+    </div>
+  )
+}
+
+export default Starred
