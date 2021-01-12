8fb138766f3eac49a67299c5fcc1f8b61f4968f6
nageswarachandru <cybernag66@gmail.com>
2021-01-12 14:07:51 +0530

    styled show pages

---
+++ src/components/show/Details.Styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+
+export const SeasonsWrapper = styled.div`
+  p {
+    margin: 5px 0;
+  }
+
+  span {
+    font-weight: 700;
+  }
+`;
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -7,6 +7,7 @@ import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.Styled';
 
 const reducer = (prevstate,action) => {
   switch(action.type){
@@ -58,28 +59,28 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData 
       image = {show.image} 
       name = {show.name} 
       rating ={show.rating} 
       summary = {show.summary} 
       tags = {show.genres} />
-      <div>
+      <InfoBlock>
         <h2> Details </h2>
         <Details status = {show.status} network = {show.network} premiered = {show.premiered} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2> Seasons </h2>
         <Seasons seasons = {show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2> Cast </h2>
         <Cast cast = {show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
